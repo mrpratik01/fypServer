f1ef1c6fff60a74a34ce6f58daedd90f4a7dded6
mrpratik01 <bhattaraipratik663@gmail.com>
2023-02-21 20:47:26 +0545

    21feb

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -18,8 +18,31 @@ router.post('/signup', async(req, res) => {
     const token = jwt.sign({user_id:user._id},jwtKey)
     res.send({token})
 
+    res.status(201).json({
+      status: "success",
+      data: {
+        packages: results.rows[0],
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+})
 
-    
+
+router.post('/login', async (req, res) => {
+  const username = req.body.username;
+  
+
+  try {
+    const results = await db.query(
+      "INSERT INTO accounts ( user_id,username, password, email) values ( $1, $2, $3, $4) returning *",
+      [req.body.user_id, req.body.username, req.body.password, req.body.email]
+    );
+    console.log(results);
+
+    const token = jwt.sign({user_id:user._id},jwtKey)
+    res.send({token})
 
     res.status(201).json({
       status: "success",
@@ -30,6 +53,7 @@ router.post('/signup', async(req, res) => {
   } catch (err) {
     console.log(err);
   }
+
 })
 
 module.exports = router;
