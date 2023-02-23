7ba8985637d3321177425ea51ecc7b4c5c5eb865
mrpratik01 <bhattaraipratik663@gmail.com>
2023-02-23 19:50:13 +0545

    23feb

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -46,11 +46,41 @@ router.post('/login', async (req, res) => {
   //     }
   // });
 
-  
+  // const { username, password } = req.body;
+
+  // db.query('SELECT * FROM users WHERE username = $1 AND password = $2', [username, password], (error, results) => {
+
+  //   if (error) {
+  //     console.error(error);
+  //     res.status(500).send('Error querying database');
+  //   }else if (results.rows.length === 0) {
+  //     res.status(401).send('Invalid credentials');
+  // } else {
+  //     // User is authenticated, so create a JWT and send it back to the client
+  //     const userId = results.rows[0].id; // assume user ID is stored in a column called "id"
+  //     const token = jwt.sign({ userId }, secretKey);
+  //     res.json({ token });
+  // }
+  // }
 
 
+    const { username, password } = req.body;
+    // Query the database for the user with the given username and password
+    pool.query('SELECT * FROM users WHERE username = $1 AND password = $2', [username, password], (error, results) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send('Error querying database');
+        } else if (results.rows.length === 0) {
+            res.status(401).send('Invalid credentials');
+        } else {
+            // User is authenticated, so create a session or token and send it back to the client
+            const sessionId = generateSessionId(); // some function to generate a session ID
+            res.json({ sessionId });
+        }
+    });
 
-  
+
+    const 
 
 
   // try {
